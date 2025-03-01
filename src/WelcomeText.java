bbd00d714bf1dea8f402bbacf34ce104654b7021
Innocentia <innocentiahan@gmail.com>
2025-03-01 20:43:39 +0800

    add files to git

---
+++ src/WelcomeText.java
@@ -0,0 +1,15 @@
+class WelcomeText {
+    public static void welcome(){
+        System.out.println("\n***********************\n");
+        System.out.println("  Daily Task Manager\n");
+        System.out.println("***********************\n");
+    }
+
+    public static void listTask(){
+        System.out.println("1. Check Email ");
+        System.out.println("2. Attend Lecture");
+        System.out.println("3. Exercise");
+        System.out.println("4.");
+        System.out.println("5.");
+    }
+}
