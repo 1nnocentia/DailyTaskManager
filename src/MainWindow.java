9bc8670b367ce338cece0c4ea69d20b601c3c892
Innocentia <innocentiahan@gmail.com>
2025-03-02 17:40:13 +0800

    buat kelas untuk main windownya

---
+++ src/MainWindow.java
@@ -0,0 +1,10 @@
+public class MainWindow {
+    static void mainWindow() {
+        System.out.println("\n     Daily Task Manager     ");
+        System.out.println("     *******************\n");
+        System.out.println("1. Create Today To-do List");
+        System.out.println("2. View Today To-do List");
+        System.out.println("3. Add New Task");
+        System.out.println("4. Exit");
+    }
+}
