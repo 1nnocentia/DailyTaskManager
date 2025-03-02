ac662666a045d01b32c6e7dc30b0a6f7104aee2c
Innocentia <innocentiahan@gmail.com>
2025-03-02 17:41:22 +0800

    update main file program

--- src/DailyTaskManager.java
+++ src/DailyTaskManager.java
@@ -8,8 +8,29 @@ import java.util.Scanner;
 public class DailyTaskManager {
 
     public static void main(String[] args) throws Exception {
+        //Declare Variable
+        int optionChoice;
+
+        Scanner scanner = new Scanner(System.in);
+
         //WelcomeText.show();
-        System.out.println("     Daily Task Manager     ");
-        System.out.println("     *******************");
+        MainWindow.mainWindow();
+        while (true) {   
+            try {
+                System.out.print("Choose an option (1-4): ");
+                optionChoice = scanner.nextInt();
+
+                // Validasi input menggunakan class InputValidation
+                InputValidation.optionValid(optionChoice);
+
+                break; // Keluar dari loop jika input valid
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage()); // Pesan error dari InputValidation
+                scanner.nextLine(); // Bersihkan buffer input agar tidak loop tanpa henti
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please enter a number between 1 and 4.");
+                scanner.nextLine(); // Bersihkan buffer input
+            }
+        }
+        }
     }
-}
